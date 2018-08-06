846cbfd0a06c0e9ca1336ef30781a43470b1315a
wooze <vagrant@homestead>
2018-08-06 06:30:25 +0000

    layout init

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-md  navbar-light">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#app-navbar-collapse">
+                &#9776;
+            </button>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Laravel Shop
+            </a>
+        </div>
+        <div class="collapse" id="app-navbar-collapse">
+            <ul class="nav navbar-nav">
+            </ul>
+            <ul class="nav navbar-nav pull-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">登录</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">注册</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
