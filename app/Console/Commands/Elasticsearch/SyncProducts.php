02371845a886dfe2acb01c5efa7e2f051410bc82
wooze <vagrant@homestead>
2018-11-02 08:14:56 +0000

    sync product to es

---
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands\Elasticsearch;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class SyncProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'es:sync-products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '同步数据到Elasticsearch';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $es = app('es');
+
+        Product::query()
+            ->with(['skus', 'properties'])
+            ->chunkById(100, function ($products) use ($es) {
+                $this->info(sprintf('正在同步 ID 范围为 %s 至 %s 的商品', $products->first()->id, $products->last()->id));
+
+                $req = ['body' => []];
+
+                foreach ($products as $product) {
+                    $data = $product->toESArray();
+
+                    //第一个参数为es操作
+                    $req['body'][] = [
+                        'index' => [
+                            '_index' => 'products',
+                            '_type' => '_doc',
+                            '_id' => $data['id'],
+                        ]
+                    ];
+                    //第二行为数据行
+                    $req['body'][] = $data;
+                }
+                try {
+                    //批量创建
+                    //用一次API完成一批操作，提高性能
+                    $es->bulk($req);
+                } catch (\Exception $e) {
+                    $this->error($e->getMessage());
+                }
+            });
+        $this->info('同步完成');
+    }
+}
+
